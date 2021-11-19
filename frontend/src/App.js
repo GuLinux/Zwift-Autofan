eee35b2d168690b360b85c08f3dcd347609021d4
Marco Gulino <marco.gulino@gmail.com>
2021-11-19 08:41:02 +0000

    Initial frontend development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Tab, Container, Navbar, Nav } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [activeKey, setActiveKey] = useState("dashboard");
+    const setTab = tabKey => () => setActiveKey(tabKey);
+
+    return <Tab.Container activeKey={activeKey}>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container>
+                <Navbar.Brand href='#home'>
+                     <img alt='' src={logo} width='30' height='30' className='d-inline-block align-top' />{' '} Zwift-Autofan
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Item>
+                            <Nav.Link href='#' onClick={setTab('dashboard')}>Dashboard</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href='#' onClick={setTab('zwift')}>Zwift</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href='#' onClick={setTab('fan')}>Fan</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href='#' onClick={setTab('buttons')}>Buttons</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Container>
+            <Tab.Content>
+                <Tab.Pane eventKey='dashboard'>
+                    <p>Dashboard</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey='zwift'>
+                    <p>Zwift</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey='fan'>
+                    <p>Fan</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey='buttons'>
+                    <p>Buttons</p>
+                </Tab.Pane>
+            </Tab.Content>
+        </Container>
+    </Tab.Container>
 }
-
 export default App;
