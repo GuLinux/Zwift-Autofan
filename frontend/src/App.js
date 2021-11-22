b112b339a3949924311d250ae1b78658a1eb82f5
Marco Gulino <marco.gulino@gmail.com>
2021-11-22 22:47:06 +0000

    Toggle navbar expansion when clicking a navlink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from './logo.png';
 import './App.css';
 import { Tab, Container, Navbar, Nav } from 'react-bootstrap';
@@ -12,20 +12,25 @@ import { FanPanel } from './fan';
 const App = () => {
     const path = useSelector((state) => state.navigation.path)
     const dispatch = useDispatch()
+    const [navExpanded, setNavExpanded] = useState(false);
 
-    const setTab = tabKey => () => dispatch(setPath(tabKey));
+    const setTab = tabKey => () => {
+        dispatch(setPath(tabKey));
+        setNavExpanded(false);
+    }
     useEffect( () => {
         console.log('Registering fetch status hook');
         dispatch(fetchBackendStatus());
     }, [dispatch]);
 
+
     return <Tab.Container activeKey={path}>
-        <Navbar bg='dark' variant='dark' expand='lg' collapsOnSelect className='mb-3'>
+        <Navbar bg='dark' variant='dark' expand='lg' collapsOnSelect className='mb-3' expanded={navExpanded}>
             <Container>
                 <Navbar.Brand href='#home'>
                      <img alt='' src={logo} width='30' height='30' className='d-inline-block align-top' />{' '} Zwift-Autofan
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Toggle aria-controls='basic-navbar-nav' onClick={() => setNavExpanded(navExpanded ? false : "expanded")} />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='me-auto'>
                         <Nav.Item>
